c1ff04564c9110e285dddb214065a3d2fd78ab1e
imjade <xiaotaoapi@163.com>
2016-09-21 09:50:13 +0800

    init

---
+++ JenkinsDemo/JenkinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsDemo
+//
+//  Created by Jade on 16/9/21.
+//  Copyright © 2016年 Jade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
